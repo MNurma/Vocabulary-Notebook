1a4774a59b04c4d5c33e70a6be0061c8b947b324
MNurma <kumauma29official@gmail.com>
2024-05-09 18:42:14 +0700

    first commit

---
+++ practice.py
@@ -0,0 +1,9 @@
+import requests
+
+api_key = '6b5894be-614c-41d0-9242-13ec18f1f475'
+word = 'voluminous'
+root_url = 'https://www.dictionaryapi.com/api/v3/references/collegiate/json'
+final_url = f'{root_url}/{word}?key={api_key}'
+r = requests.get(final_url)
+result = r.json()
+print(result)
